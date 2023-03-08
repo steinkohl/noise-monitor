b8a908f77da2417a10eba8596f2f7eae1ba5a3c6
Felix Steinkohl <steinkohl@campus.tu-berlin.de>
2023-03-08 07:06:38 +0100

    added delay feature

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="noise_monitor",
-    version="0.2.1",
+    version="0.2.2",
     description="A python package to control ground stations and perform measurements",
     author="Felix Steinkohl",
     author_email="steinkohl@campus.tu-berlin.de",
