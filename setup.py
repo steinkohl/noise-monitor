614982eb74d8903bf5792106c07db9dcad7c1220
Felix Steinkohl <steinkohl@campus.tu-berlin.de>
2023-03-12 16:34:13 +0100

    added descriptions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="noise_monitor",
-    version="0.3.1",
+    version="0.3.2",
     description="A python package to control ground stations and perform measurements",
     author="Felix Steinkohl",
     author_email="steinkohl@campus.tu-berlin.de",
